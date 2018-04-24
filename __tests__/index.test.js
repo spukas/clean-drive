8450bef74657479a464efdd9575112940ac44de8
Linas Spukas <l.spukas@gmail.com>
2018-04-24 23:03:55 +0300

    Set up testing environment with jest

---
+++ __tests__/index.test.js
@@ -0,0 +1,32 @@
+/* eslint-env jest */
+
+import { shallow } from 'enzyme';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+const App = () => (
+    <div>
+        <style jsx>{`
+            p {
+                color: red;
+            }
+        `}</style>
+        <p>Hello World!</p>
+    </div>
+);
+
+describe('With Enzyme', () => {
+    it('App shows "Hello world!"', () => {
+        const app = shallow(<App />);
+
+        expect(app.find('p').text()).toEqual('Hello World!');
+    });
+});
+
+describe('With Snapshot Testing', () => {
+    it('App shows "Hello world!"', () => {
+        const component = renderer.create(<App />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
