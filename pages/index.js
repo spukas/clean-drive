d4cdc49e167267ddf043f2dccbec5c0d035cda22
Linas Spukas <l.spukas@gmail.com>
2018-04-25 21:35:21 +0300

    Setup eslint and prettier

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import { theme } from '../src/theme';
 import { Button } from '../components';
 
 export default () => (
-    <ThemeProvider theme={theme}>
-        <React.Fragment>
-            <Button color="primary">Normal</Button>
-            <Button color="secondary">Themed</Button>
-        </React.Fragment>
-    </ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <Button color="primary">Normal</Button>
+      <Button color="secondary">Themed</Button>
+    </React.Fragment>
+  </ThemeProvider>
 );
